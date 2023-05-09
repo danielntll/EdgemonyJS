3da412c97926cecf7b45fd90e05ae6fc734b0cfb
danielntll <116676924+danielntll@users.noreply.github.com>
2023-05-09 16:19:04 +0200

    09-05-2023

---
+++ 09-05-2023/script.js
@@ -0,0 +1,57 @@
+// -------------- es 1 -------------------------
+const dinamicTxt = document.querySelector(".dinamicTxt");
+
+const btnEs1 = document.querySelector(".btnEs1");
+
+btnEs1.addEventListener("click", () => {
+  let newNumber = Math.floor(Math.random() * 100);
+  dinamicTxt.textContent = newNumber + " Ciao!";
+});
+
+
+// -------------- es 2 -------------------------
+const form = document.querySelector("#form");
+
+const respTxt = document.querySelector(".respTxt");
+const clearRespTxt = document.querySelector(".clearRespTxt");
+
+form.addEventListener("submit", ev => {
+  ev.preventDefault();
+  if (ev.target[0].value.length === 0) {
+    respTxt.textContent = "Proprio zero pensieri? Fortunato te!";
+  } else {
+    respTxt.textContent = "Sul serio hai pensato a " + ev.target[0].value + "??";
+    ev.target[0].value = "";
+  }
+
+});
+
+clearRespTxt.addEventListener("click", () => {
+  respTxt.textContent = "";
+})
+
+// -------------- es 3 -------------------------
+
+// Ref --------------
+const inputList = document.querySelector(".inputList");
+const btnAdd = document.querySelector(".btnAdd");
+const elementsList = document.querySelector(".elList");
+
+// Conditions -------
+btnAdd.addEventListener("click", () => {
+  buildNewElem(inputList.value);
+  inputList.value = ""; //clear input value;
+});
+
+function buildNewElem(txtValue) {
+  const newElem = document.createElement("li");
+  newElem.textContent = txtValue;
+  newElem.addEventListener("click", () => {
+    newElem.classList.toggle("completed")
+  });
+  newElem.addEventListener("dblclick", () => {
+    newElem.remove();
+  });
+
+  elementsList.appendChild(newElem);
+}
