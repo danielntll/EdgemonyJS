6a780095ffa8248e29297d6f8cbb79c1b383eb04
danielntll <dev@danielntll.it>
2023-05-24 17:25:54 +0200

    Update script.js

--- 24-05-2023/Esercizi/script.js
+++ 24-05-2023/Esercizi/script.js
@@ -6,9 +6,6 @@
 
 // console.log(nome, cognome, paese)
 
-
-
-
 // Esercizio 2 -----------------
 
 // let libro = {
@@ -21,25 +18,29 @@
 
 // console.log(titolo, autore, anno);
 
-
-
-
-
 // Avanzato 1 ------------------
 
 let libro = {
   titolo: "Il Nome della Rosa",
   autore: "Umberto Eco",
-  anno: 1980
+  anno: 1980,
 };
 
-let { titolo, autore, anno = "Non specificato" } = libro;
+let {
+  titolo = "Se non c'è il titolo c'è un errore di Upload x(",
+  autore = "Non ho previsto che non ci fosse l'autore x(",
+  anno = "Non specificato",
+  nuovaKey = "Non sono sicuro che tutti i libri abbiano questa nuova key",
+} = libro;
 
 console.log(titolo, autore, anno);
 
-
 let persona = ["Mario", "Rossi", "Italia"];
 
-let [nome, cognome, paese = "Non specificato"] = persona;
+let [
+  nome = "Non specificato",
+  cognome = "Non specificato",
+  paese = "Non specificato",
+] = persona;
 
-console.log(nome, cognome, paese)
+console.log(nome, cognome, paese);
